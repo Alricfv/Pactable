74b0c31e0b5376dfa4be9f977e1f875f07b781d0
Alricfv <alricmarvel@gmail.com>
2025-08-30 04:53:21 +0530

    removed useless imports, gonna implement the signout button somewhere tho dw

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { createClient } from '@/lib/supabaseClient'
-import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 
 let authPromise: Promise<any> | null = null;
