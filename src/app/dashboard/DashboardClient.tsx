53f27741cb6006add95e8c44a0930805dc0290f6
Alricfv <alricmarvel@gmail.com>
2025-09-02 22:09:33 +0530

    okay well that was a bit tuff, i added the delete functionality and was doing alot of troubleshooting because i had to make the code modular for it to make sense. It was getting very disorganized. All fixed though!

--- src/app/dashboard/DashboardClient.tsx
+++ src/app/dashboard/DashboardClient.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { FileText, PlusCircle, MoreVertical } from 'lucide-react';
+import { FileText, PlusCircle, MoreVertical, Trash2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
+import { createClient } from '@/lib/supabaseClient';
 
 type Participant ={
     id: string;
@@ -41,7 +42,6 @@ const AgreementPreview = ({ title, content }: {title: string, content: string | 
                         <p key={index} className="ml-1 truncate">
                             {line}
                         </p>
-                    
                 )}
                 return <p key={index} className="truncate">{line}</p>
             })}
@@ -49,17 +49,57 @@ const AgreementPreview = ({ title, content }: {title: string, content: string | 
     );
 };
 
-export default function DashboardClient({agreements, userId} : {agreements: Agreement[], userId: string }) {
-    const [isClient, setIsClient] = useState(false);
+export default function DashboardClient({agreements: initialAgreements, userId} : {agreements: Agreement[], userId: string }) {
 
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
+    const [error, setError] = useState<string | null>(null);
+    const supabase = createClient();
+    const [agreements, setAgreements] = useState(initialAgreements);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [agreementToDelete, setAgreementToDelete] = useState<Agreement | null>(null);
+    
+    const handleDeleteAgreement = async (agreementId: string) => {
+        setError(null);
+        const{ error: rpcError } = await supabase.rpc('delete_agreement',{
+             agreement_id: agreementId 
+        });
+
+        if (rpcError){
+            console.error('Failed to delete agreement:', rpcError);
+            setError(`Error: ${rpcError.message}`);
+        }
+        else{
+            setAgreements(currentAgreements => currentAgreements.filter(a => a.id !== agreementId));
+        }
+    }
+
+    const handleOpenModal = (agreement: Agreement) =>{
+        setAgreementToDelete(agreement);
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setAgreementToDelete(null);
+    };
+
+    const handleConfirmDelete = () => {
+        if (agreementToDelete) {
+            handleDeleteAgreement(agreementToDelete.id);
+        }
+        handleCloseModal();
+    }
 
     const createdAgreements = agreements.filter(a => a.created_by === userId);
     const receivedAgreements = agreements.filter(a => a.created_by !== userId);
     return(
         <>
+            <ConfirmationModal
+                isOpen={isModalOpen}
+                onCancel={handleCloseModal}
+                onConfirm={handleConfirmDelete}
+                title={agreementToDelete?.title || ''}
+            />
+
             <div className="w-full px-6 sm:px-10 space-y-12">
                 <div className="text-center mb-12">
                     <h1 className="text-5xl font-bold text-white">
@@ -69,106 +109,134 @@ export default function DashboardClient({agreements, userId} : {agreements: Agre
                         Let's agree on stuff shall we?
                     </p>
                 </div>
-                <div>
-                    <div className="flex justify-between items-center mb-6">
-                        <h2 className="text-4xl font-semibold text-white">
-                            Your Agreements
-                        </h2>
-                    </div>
 
-                    {createdAgreements.length > 0 ? (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                            {createdAgreements.map((agreement) => (
-                                <div key={agreement.id} className="group relative rounded-xl p-px bg-gradient-to-br from-white/10 via-white/5 to-transparent transition-all duration-300 hover:from-white/20 hover:via-white/10">
-                                    <div className="relative h-full w-full rounded-[11px] bg-[#09090b] p-3">
-                                        <a
-                                            href={`/dashboard/agreements/${agreement.id}`}
-                                            className={`block h-40 bg-[#0f0f0f] rounded-lg border-2 border-[#262626] hover:border-grey-50 transition flex items-center justify-center `}
-                                        >
-                                            <AgreementPreview title={agreement.title} content={agreement.content}/>
-                                        </a>
-                                        <div className="mt-3">
-                                            <h3 className="text-md font-semibold text-white truncate">
-                                                {agreement.title}
-                                            </h3>
-                                            <div className="flex justify-between items-center">
-                                                <p className="text-sm text-gray-400">
-                                                    {isClient ? new Date(agreement.created_at).toLocaleDateString() : 'Loading...'}
-                                                </p>
-                                                <button
-                                                    onClick={(e) => { e.preventDefault(); alert('Options Menu!'); }}
-                                                    className="text-gray-500 hover:text-white opacity-0 group-hover:opacity-100 transition"
-                                                >
-                                                    <MoreVertical size={18} />
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ): (
-                        <div className="text-center py-16 px-6 bg-[#0f0f0f] rounded-lg border border-dashed border-[#262626]">
-                            <h3 className="text-xl font-semibold text-white">
-                                Get on your first agreement ASAP!
-                            </h3>
-                            <p className="">
-                                You haven't created / been added to agreements yet!
-                            </p>
+                {error && <p className="text-center bg-red-50 text-red-500 p-3 rounded-md">{error}</p>}
+
+                {agreements.length > 0 ? (
+                    <div className="space-y-10">
+                        <div className="flex items-center justify-between mb-6">
+                            <h2 className="text-3xl font-semibold text-white mb-6">
+                                Your Agreements
+                            </h2>
                             <a
                                 href="/dashboard/agreements"
-                                className="mt-6 inline-flex items-center gap-2 bg-gray-50 text-white rounded-md px-4 py-2 font-semibold hover:bg-gray-100 transition"
+                                className="inline-flex items-center gap-2 bg-gray-50 text-gray-950 rounded-md px-4 py-2 font-semibold hover:bg-gray-100 transition text-base"
                             >
-                                <PlusCircle size={18} className="text-gray-950" />
-                                <span className="text-gray-950">Create your first agreement here!</span>
+                                <PlusCircle size={20} />
+                                <span> New Agreement </span>
                             </a>
+                            <AgreementGrid agreements={createdAgreements} onDelete={handleOpenModal}/>
                         </div>
-                    )}
-                </div>
-            </div>
-            <div>
-                <div className="flex justify-between py-10 items-center ">
-                    <h2 className="text-4xl font-semibold text-white px-10">
-                        Received Agreements
-                    </h2>
-                </div>
-                {receivedAgreements.length > 0 ? (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-                        {receivedAgreements.map((agreement) =>(
-                            <div key={agreement.id} className="group relative rounded-xl p-px bg-gradient-to-br from-white/10 via-white/5 to-transparent transition-all duration-300 hover:from-white/20 hover:via-white/10">
-                                <div className="relative h-full w-full rounded-[11px] bg-[#09090b] p-3">
-                                    <a
-                                        href={`/dashboard/agreements/${agreement.id}`}
-                                        className="block h-40 bg-[#0f0f0f] rounded-lg border-2 border-[#262626] hover:border-grey-50 transition flex items-center justify-center"
-                                    >
-                                        <AgreementPreview title={agreement.title} content={agreement.content} />
-                                    </a>
-                                    <div className="mt-3">
-                                        <h3 className="text-md font-semibold text-white truncate">
-                                            {agreement.title}
-                                        </h3>
-                                        <div className="flex justify-between items-center">
-                                            <p className="text-sm text-gray-400">
-                                                {isClient ? new Date(agreement.created_at).toLocaleDateString(): ''}
-                                            </p>
-                                            <button
-                                                onClick={(e) => {e.preventDefault(); alert('Options Menu!'); }}
-                                                className="text-gray-500 hover:text-white opacity-0 group-hover:opacity-100 transition"
-                                            >
-                                                <MoreVertical size={18}/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                        <div>
+                            <h2 className="text-3xl font-semibold text-white mb-6">
+                                Received Agreements
+                            </h2>
+                            <AgreementGrid agreements={receivedAgreements} />
+                        </div>
                     </div>
-                ):(
-                    <p className="px-10">
-                        No agreements have been sent to you just yet.
-                    </p>
+                ) : (
+                    <div className="text-center py-16 px-6 bg-[#0f0f0f] rounded-lg border border-dashed border-[#262626]">
+                        <h3 className="text-xl font-semibold text-white">
+                            Get on your first agreement ASAP!
+                        </h3>
+                        <p className="">
+                            You haven't created / been added to agreements yet!
+                        </p>
+                        <a
+                            href="/dashboard/agreements"
+                            className="mt-6 inline-flex items-center gap-2 bg-gray-50 text-white rounded-md px-4 py-2 font-semibold hover:bg-gray-100 transition"
+                        >
+                            <PlusCircle size={18} className="text-gray-950" />
+                            <span className="text-gray-950">Create your first agreement here!</span>
+                        </a>
+                    </div>
                 )}
             </div>
         </>
+    );
+}
+
+function ConfirmationModal({isOpen, onCancel, onConfirm, title}:{isOpen: boolean; onCancel: () => void; onConfirm: ()=> void; title: string;}){
+    if (!isOpen) 
+        return null;
+
+    return (
+        <div className={`flex justify-center items-center p-4 fixed inset-0 bg-black bg-opacity-60 z-50 backdrop-blur-sm transition-opacity duration-500 
+                        ${isOpen? 'opacity-100 pointer-events-auto' : 'opacit-0 pointer-events-none'}`}>
+            <div className={`bg-[#000000] p-6 rounded-lg shadow-xl max-w-sm w-full border border-gray-700 
+                            transform transition-all duration-300 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
+                <h3 className="text-lg font-bold text-white">
+                    Confirm Deletion 
+                </h3>
+                <p className="text-gray-100 my-4">
+                    Are you sure you want to delete "{title}"? This action is permanent!
+                </p>
+                <div className="flex justify-end gap-4 mt-6">
+                    <button
+                        onClick={onCancel}
+                        className="px-4 py-2 rounded-md bg-white text-gray-950 hover:bg-gray-50"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={onConfirm}
+                        className="px-4 py-2 rounded-md bg-red-600 text-white hover:bg-red-500 transition"
+                    >
+                        Confirm Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function AgreementGrid({ agreements, onDelete }: { agreements: Agreement[], onDelete?: (agreement: Agreement) => void}){
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {setIsClient(true); }, []);
+
+    if (agreements.length === 0) {
+        return(
+            <p className="text-gray-500 px-10 sm:px-0">
+                No agreements found.
+            </p>
+        )
+    }
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+            {agreements.map((agreement) => (
+                <div key={agreement.id} className="group relative rounded-xl bg-gradient-to-br from-white/10 via-white/5 to-transparent transition-all duration-300 hover:from-white/20 hover:via-white/10">
+                    <div className="relative h-full w-full rounded-[11px] bg-[#09090b] p-3">
+                        <a
+                            href={`/dashboard/agreements/${agreement.id}`}
+                            className="block h-40 bg-[#0f0f0f] rounded-lg border-2 border-[#262626] hover:border-gray-500 transition overflow-hidden"
+                        >
+                            <AgreementPreview title={agreement.title} content={agreement.content}/>
+                        </a>
+                        <div className="mt-3">
+                            <h3 className="text-md font-semibold text-white truncate">
+                                {agreement.title}
+                            </h3>
+                            <div className="flex justify-between items-center">
+                                <p>
+                                    {isClient ? new Date(agreement.created_at).toLocaleDateString() : 'Loading...'}
+                                </p>
+                                {onDelete && (
+                                    <button
+                                        onClick={(e) => { 
+                                            e.preventDefault();
+                                            onDelete(agreement);
+                                        }}
+                                        className="flex items-center gap-1 text-red-500 hover:text-red-400 opacity-0 group-hover:opacity-100 transition text-xs"
+                                    >
+                                        <Trash2 size={14} />
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
